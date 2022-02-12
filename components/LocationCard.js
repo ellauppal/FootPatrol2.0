7c2cffb054f968730059587f26e32a0d3791defa
Ella Uppal <ellauppal@gmail.com>
2022-02-12 14:10:03 -0500

    Home map page complete

--- components/LocationCard.js
+++ components/LocationCard.js
@@ -1,83 +1,118 @@
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from 'react';
-import { MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-  
-  export default function Card() {
-    return (
-        <View style={styles.rectangle}>
-            <View style={styles.head}>
-                <Text style={styles.header}>Current Location</Text>
-                <Text style={styles.subHeader}>106 Seagram Dr, Waterloo ON</Text>
-            </View>
-            <View style={styles.below}>
-                <View style={styles.inLine}>
-                    <MaterialIcons style={styles.icon} name="my-location" size={24} color="#878181"/>
-                    <TextInput style={styles.input} placeholder='  Enter your pick-up point'/>
+
+
+    export default class MapView extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.state = { dropoff: '' };
+            this.state = { pickup: '' };
+        }
+
+        render(){
+
+            return (
+                <View style={styles.rectangle}>
+                    <View style={styles.head}>
+                    <Text style={styles.header}>Current Location</Text>
+                    <Text style={styles.subHeader}>106 Seagram Dr, Waterloo ON</Text>
+                    </View>
+                    <View style={styles.below}>
+                    <View style={styles.inLine}>
+                        <MaterialIcons
+                        style={styles.icon}
+                        name='dropoff'
+                        name='my-location'
+                        size={24}
+                        color='#878181'
+                        />
+                        <TextInput
+                        style={styles.input}
+                        placeholder='  Enter your pick-up point'
+                        name='pickup'
+                        {...this.props}
+                        value={this.state.pickup}
+                        onChangeText={ (pickup) => this.setState({ pickup }) }
+                        />
+                    </View>
+                    <View style={styles.inLine}>
+                        <Ionicons
+                        style={styles.icon}
+                        name='location-sharp'
+                        size={24}
+                        color='#878181'
+                        />
+                        <TextInput
+                        {...this.props}
+                        style={styles.input}
+                        placeholder='  Enter your drop-off point'
+                        value={this.state.dropoff}
+                        onChangeText={ (dropoff) => this.setState({ dropoff }) }
+                        />
+                    </View>
+                    </View>
                 </View>
-                <View style={styles.inLine}>
-                    <Ionicons style={styles.icon} name="location-sharp" size={24} color="#878181"/>
-                    <TextInput style={styles.input} placeholder='  Enter your drop-off point'/>
-                </View>
-            </View>       
-        </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
+                );
+            }
+    }
+
+    const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'relative',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
     },
     rectangle: {
-      height: 148,
-      width: 370,
-      borderRadius:25,
-      borderColor: '#508FED',
-      borderWidth: 1,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 1 },
-      shadowOpacity: 0.25,
-      shadowRadius: 4, 
-      backgroundColor: '#fff',
-      position: 'absolute', 
-      zIndex: 10,
-      top: '7%',
+        height: 148,
+        width: 370,
+        borderRadius: 25,
+        borderColor: '#7C63E3',
+        borderWidth: 1,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        backgroundColor: '#fff',
+        position: 'absolute',
+        zIndex: 10,
+        top: '7%',
     },
     header: {
-        fontWeight:'600'
+        fontWeight: '600',
     },
     subHeader: {
-      fontWeight:'normal'
+        fontWeight: 'normal',
     },
-    input:{
-      marginTop: '3%',
-      fontSize: 15,
-      fontWeight:'normal',
-      color: '#C4C4C4',
-      backgroundColor: '#F3F3F3',
-      height: 30,
-      width: 280
+    input: {
+        marginTop: '3%',
+        fontSize: 15,
+        fontWeight: 'normal',
+        color: '#C4C4C4',
+        backgroundColor: '#F3F3F3',
+        height: 30,
+        width: 280,
     },
     head: {
-      alignItems: 'center',
-      fontSize: 13,
-      fontFamily: 'Open Sans',
-      color: '#3C3C3C',
-      top: '10%'
+        alignItems: 'center',
+        fontSize: 13,
+        fontFamily: 'Open Sans',
+        color: '#3C3C3C',
+        top: '10%',
     },
     below: {
         alignItems: 'center',
         top: '13%',
     },
-    inLine:{
-        flexDirection: 'row'
+    inLine: {
+        flexDirection: 'row',
     },
     icon: {
         top: '3%',
-        marginRight: '3%'
-    }
-  });
+        marginRight: '3%',
+    },
+    });
--- pages/MapPage.js
+++ pages/MapPage.js
@@ -1,253 +1,323 @@
-import {
-  setStatusBarNetworkActivityIndicatorVisible,
-  StatusBar,
-} from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import React from 'react';
-import { Location, Permissions } from 'expo';
-import { useEffect } from 'react';
-import Card from '../components/LocationCard';
+import MapViewDirections from 'react-native-maps-directions';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import FooterMap from '../components/Footer';
 
-export default function App() {
-  /*
-        state = {
-            Location: {},
-            errorMessage: '',
-        };
-        
-        useEffect(() => {
-            this._getLocation();
-        }, []);
+const GOOGLE_MAPS_APIKEY = 'AIzaSyB0QhWbi7LfDb8ays7cmdJ5XT3dwTU8jFw';
 
-        _getLocation = async () => {
-            const { status } = await Permissions.askAsync(Permissions.Location);
-        
-            if (status !== 'granted'){
-                console.log('permission not granted!');
-        
-                this.setState({
-                    errorMessage: 'permission not granted'
-                })
-            }
-            const Location =  await Location.getCurrentPositionAsync();
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { dropoff: '' };
+    this.state = { pickup: '' };
+  }
 
-            this.setState({
-                Location
-            })
-        }
-    */
-    let x = true;
-
-    function myFunction() {
-        document.getElementById("myCheck").click();
-      }
-
-  return (
-    <View style={styles.container}>
-        <Card></Card>
+  render() {
+    return (
+      <View style={styles.container}>
+        <FooterMap></FooterMap>
+        <View style={styles.rectangle}>
+          <View style={styles.head}>
+            <Text style={styles.header}>Current Location</Text>
+            <Text style={styles.subHeader}>106 Seagram Dr, Waterloo ON</Text>
+          </View>
+          <View style={styles.below}>
+            <View style={styles.inLine}>
+              <MaterialIcons
+                style={styles.icon}
+                name='dropoff'
+                name='my-location'
+                size={24}
+                color='#878181'
+              />
+              <TextInput
+                style={styles.input}
+                placeholder='  Enter your pick-up point'
+                name='pickup'
+                {...this.props}
+                value={this.state.pickup}
+                onChangeText={pickup => this.setState({ pickup })}
+              />
+            </View>
+            <View style={styles.inLine}>
+              <Ionicons
+                style={styles.icon}
+                name='location-sharp'
+                size={24}
+                color='#878181'
+              />
+              <TextInput
+                {...this.props}
+                style={styles.input}
+                placeholder='  Enter your drop-off point'
+                value={this.state.dropoff}
+                onChangeText={dropoff => this.setState({ dropoff })}
+              />
+            </View>
+          </View>
+        </View>
         <MapView
-            showsMyLocationButton={true}
-            showsUserLocation={true}
-            provider={PROVIDER_GOOGLE}
-            customMapStyle={mapStyle}
-            style={{ height: '100%', width: '100%' }}
-            region={{
+          showsMyLocationButton={true}
+          showsUserLocation={true}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={mapStyle}
+          style={{ height: '95%', width: '100%', bottom: '8.2%' }}
+          region={{
             latitude: 37.78825,
             longitude: -122.4324,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-            }}
-        />
+          }}
+        >
+          <MapViewDirections
+            origin={this.state.pickup}
+            destination={this.state.dropoff}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor='#7C63E3'
+          />
+        </MapView>
         <StatusBar style='auto' />
-        </View>
-  );
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    position: 'relative'
-  }
+    position: 'relative',
+  },
+  rectangle: {
+    height: 148,
+    width: 370,
+    borderRadius: 25,
+    borderColor: '#7C63E3',
+    borderWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    zIndex: 10,
+    top: '7%',
+  },
+  header: {
+    fontWeight: '600',
+  },
+  subHeader: {
+    fontWeight: 'normal',
+  },
+  input: {
+    marginTop: '3%',
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: '#C4C4C4',
+    backgroundColor: '#F3F3F3',
+    height: 30,
+    width: 280,
+  },
+  head: {
+    alignItems: 'center',
+    fontSize: 13,
+    fontFamily: 'Open Sans',
+    color: '#3C3C3C',
+    top: '10%',
+  },
+  below: {
+    alignItems: 'center',
+    top: '13%',
+  },
+  inLine: {
+    flexDirection: 'row',
+  },
+  icon: {
+    top: '3%',
+    marginRight: '3%',
+  },
 });
 
 const mapStyle = [
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#e9e9e9"
-            },
-            {
-                "lightness": 17
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f5f5f5"
-            },
-            {
-                "lightness": 20
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 17
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 29
-            },
-            {
-                "weight": 0.2
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 18
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f5f5f5"
-            },
-            {
-                "lightness": 21
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#dedede"
-            },
-            {
-                "lightness": 21
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "saturation": 36
-            },
-            {
-                "color": "#333333"
-            },
-            {
-                "lightness": 40
-            }
-        ]
-    },
-    {
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            },
-            {
-                "lightness": 19
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 20
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 17
-            },
-            {
-                "weight": 1.2
-            }
-        ]
-    }
-]
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e9e9e9',
+      },
+      {
+        lightness: 17,
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 17,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 29,
+      },
+      {
+        weight: 0.2,
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 18,
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 16,
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+      {
+        lightness: 21,
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dedede',
+      },
+      {
+        lightness: 21,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 16,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        saturation: 36,
+      },
+      {
+        color: '#333333',
+      },
+      {
+        lightness: 40,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f2f2f2',
+      },
+      {
+        lightness: 19,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#fefefe',
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#fefefe',
+      },
+      {
+        lightness: 17,
+      },
+      {
+        weight: 1.2,
+      },
+    ],
+  },
+];
