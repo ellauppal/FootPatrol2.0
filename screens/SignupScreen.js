d0ece6c677bf784c3a4074cc1cb9f22aba75000b
Michelle Paradis <michpara@yahoo.com>
2022-02-12 16:40:23 -0500

    changing sign up style

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button as RNButton } from 'react-native';
+import { StyleSheet, Text, TextInput,View, Button } from 'react-native';
 
-import { Button, InputField, ErrorMessage } from '../components';
+import {  InputField, ErrorMessage } from '../components';
 import Firebase from '../config/firebase';
+import { Dimensions } from 'react-native';
+import SButSignUp from '../components/SButSignUp.js'
+import BtmIcons1 from '../components/BtmIcons1.js';
+
+import Welcome1 from '../components/Welcome1.js';
+import Arrow from '../components/Arrow.js';
 
 const auth = Firebase.auth();
 
@@ -37,76 +43,115 @@ export default function SignupScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <StatusBar style='dark-content' />
-      <Text style={styles.title}>Create new account</Text>
-      <InputField
-        inputStyle={{
-          fontSize: 14
-        }}
-        containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
-        }}
-        leftIcon='email'
-        placeholder='Enter email'
-        autoCapitalize='none'
-        keyboardType='email-address'
-        textContentType='emailAddress'
-        autoFocus={true}
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <InputField
-        inputStyle={{
-          fontSize: 14
-        }}
-        containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
-        }}
-        leftIcon='lock'
-        placeholder='Enter password'
-        autoCapitalize='none'
-        autoCorrect={false}
-        secureTextEntry={passwordVisibility}
-        textContentType='password'
-        rightIcon={rightIcon}
-        value={password}
-        onChangeText={text => setPassword(text)}
-        handlePasswordVisibility={handlePasswordVisibility}
-      />
+      <StatusBar style="auto" />
+      <Arrow/>
+        
+      <Welcome1/>
+      <View style={styles.container1}>
+        <InputField
+          style={styles.input}
+          leftIcon='email'
+          placeholder='Enter email'
+          autoCapitalize='none'
+          keyboardType='email-address'
+          textContentType='emailAddress'
+          autoFocus={true}
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <View style={styles.style} />
+      </View>
+      <View style={styles.container2}>
+        <InputField
+          style={styles.input}
+          leftIcon='lock'
+          placeholder='Enter password'
+          autoCapitalize='none'
+          autoCorrect={false}
+          secureTextEntry={passwordVisibility}
+          textContentType='password'
+          rightIcon={rightIcon}
+          value={password}
+          onChangeText={text => setPassword(text)}
+          handlePasswordVisibility={handlePasswordVisibility}
+        />
+        <View style={styles.style} />
+      </View>
       {signupError ? <ErrorMessage error={signupError} visible={true} /> : null}
-      <Button
+      <View style={styles.button}>
+        <Button 
         onPress={onHandleSignup}
-        backgroundColor='#f57c00'
-        title='Signup'
-        tileColor='#fff'
-        titleSize={20}
-        containerStyle={{
-          marginBottom: 24
-        }}
-      />
-      <RNButton
-        onPress={() => navigation.navigate('Login')}
-        title='Go to Login'
-        color='#fff'
-      />
+
+        title='Sign up' 
+        color='#7C63E3'
+        style={styles.text} 
+        />
+      </View>
     </View>
   );
 }
+const {
+  width: SCREEN_WIDTH,
+  height: SCREEN_HEIGHT,
+} = Dimensions.get('window');
+const scale_w1= SCREEN_WIDTH/12
+const scale_h1=SCREEN_HEIGHT/4
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e93b81',
-    paddingTop: 50,
-    paddingHorizontal: 12
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    top: 100
+    
   },
-  title: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#fff',
-    alignSelf: 'center',
-    paddingBottom: 24
-  }
+  container1: {
+    flex: 1,
+    left: SCREEN_WIDTH/9.9,
+    bottom:40
+  },
+  container2: {
+    flex: 1,
+    left: SCREEN_WIDTH/9.9,
+    bottom: 110
+  },
+  container3: {
+    
+    flex: 1,
+    left: SCREEN_WIDTH/9.9,
+    bottom: 180
+  },
+  container4: {
+    
+    flex: 1,
+    left: SCREEN_WIDTH/9.9,
+    marginBottom: -160,
+    bottom: 250
+    
+  },
+  input: {
+    fontSize: 20,
+    width: 300,
+    marginBottom: 5,
+  },
+  style: {
+        alignItems:'flex-start',
+        justifyContent:'center',
+        borderBottomColor: '#7C63E3',
+        borderBottomWidth: 2,
+        width: 300,
+  },
+      button: {
+        flex: 1,
+        alignItems:'flex-start',
+        justifyContent:'center',
+        fontSize: 20,
+        left: scale_w1,
+        bottom: scale_h1,
+    },
+    text: {
+        fontSize: 20
+    }
 });
+
