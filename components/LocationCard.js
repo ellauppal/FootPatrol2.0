ebf499b85054f00aa9f1e0bc5f8595cdd8b671a4
Ella Uppal <ellauppal@gmail.com>
2022-02-12 09:50:46 -0500

    Update map colour and add location component

---
+++ components/LocationCard.js
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
+  
+  export default function Card() {
+    return (
+        <View style={styles.rectangle}>
+            <View style={styles.head}>
+                <Text style={styles.header}>Current Location</Text>
+                <Text style={styles.subHeader}>106 Seagram Dr, Waterloo ON</Text>
+            </View>
+            <View style={styles.below}>
+                <View style={styles.inLine}>
+                    <MaterialIcons style={styles.icon} name="my-location" size={24} color="#878181"/>
+                    <TextInput style={styles.input} placeholder='  Enter your pick-up point'/>
+                </View>
+                <View style={styles.inLine}>
+                    <Ionicons style={styles.icon} name="location-sharp" size={24} color="#878181"/>
+                    <TextInput style={styles.input} placeholder='  Enter your drop-off point'/>
+                </View>
+            </View>       
+        </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'relative',
+    },
+    rectangle: {
+      height: 148,
+      width: 370,
+      borderRadius:25,
+      borderColor: '#508FED',
+      borderWidth: 1,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.25,
+      shadowRadius: 4, 
+      backgroundColor: '#fff',
+      position: 'absolute', 
+      zIndex: 10,
+      top: '7%',
+    },
+    header: {
+        fontWeight:'600'
+    },
+    subHeader: {
+      fontWeight:'normal'
+    },
+    input:{
+      marginTop: '3%',
+      fontSize: 15,
+      fontWeight:'normal',
+      color: '#C4C4C4',
+      backgroundColor: '#F3F3F3',
+      height: 30,
+      width: 280
+    },
+    head: {
+      alignItems: 'center',
+      fontSize: 13,
+      fontFamily: 'Open Sans',
+      color: '#3C3C3C',
+      top: '10%'
+    },
+    below: {
+        alignItems: 'center',
+        top: '13%',
+    },
+    inLine:{
+        flexDirection: 'row'
+    },
+    icon: {
+        top: '3%',
+        marginRight: '3%'
+    }
+  });
--- pages/MapPage.js
+++ pages/MapPage.js
@@ -1,14 +1,17 @@
-import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  setStatusBarNetworkActivityIndicatorVisible,
+  StatusBar,
+} from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import React from 'react';
 import { Location, Permissions } from 'expo';
-import { useEffect } from "react";
-
+import { useEffect } from 'react';
+import Card from '../components/LocationCard';
 
 export default function App() {
-    /*
+  /*
         state = {
             Location: {},
             errorMessage: '',
@@ -35,19 +38,26 @@ export default function App() {
             })
         }
     */
-    return (
-        
-        <View style={styles.container}>
-        <Text>Map Page</Text>
+    let x = true;
+
+    function myFunction() {
+        document.getElementById("myCheck").click();
+      }
+
+  return (
+    <View style={styles.container}>
+        <Card></Card>
         <MapView
+            showsMyLocationButton={true}
             showsUserLocation={true}
             provider={PROVIDER_GOOGLE}
-            style={{ height: '50%', width: '100%' }}
+            customMapStyle={mapStyle}
+            style={{ height: '100%', width: '100%' }}
             region={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
             }}
         />
         <StatusBar style='auto' />
@@ -61,5 +71,183 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    position: 'relative'
+  }
 });
+
+const mapStyle = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
