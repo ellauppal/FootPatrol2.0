1b37ae321ed3fe828ec5176af9784c1f3acd524c
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2022-02-12 14:46:23 -0500

    Add toggle button

---
+++ components/ToggleList.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ToggleList() {
+  return (
+    <View style={styles.togglebackground}>
+      <View style={styles.toggle}>
+        <Text style={styles.inactive} onPress={() => console.log()}>Map</Text>
+        <View style={styles.activebackground}>
+          <Text style={styles.active}>List</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  togglebackground: {
+    backgroundColor: '#7C63E3',
+    width: 260,
+    height: 48,
+    justifyContent: "center",
+    borderRadius: 33,
+    position: 'absolute',
+    top: 40,
+    alignSelf: 'center',
+  },
+  toggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  inactive: {
+    color: 'white',
+    fontSize: 18,
+    width: 122,
+    textAlign: 'center',
+    paddingTop: 5,
+  },
+  activebackground: {
+    backgroundColor: 'white',
+    borderRadius: 33,
+    height: 33,
+    justifyContent: 'center',
+  },
+  active: {
+    color: '#3C3C3C',
+    fontSize: 18,
+    width: 122,
+    textAlign: 'center',
+  },
+})
