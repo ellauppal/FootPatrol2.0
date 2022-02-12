14c8998bea9e1c257f04fee871aab13ecb3abf9e
Ella Uppal <ellauppal@gmail.com>
2022-02-11 21:15:06 -0500

    First Commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Map Page</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
