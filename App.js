b55383db58e1105f772915ce1ca5c28d507ceb32
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2022-02-12 20:04:28 -0500

    Change patrollee image

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import PatrolToStart from './pages/PatrolToStart.js';
 import FooterMap from './components/FooterMap.js';
-import Image from './assets/image.jpg';
+import JaneDoeImage from './assets/JaneDoe.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <PatrolToStart firstName="Victoria" lastName="Da Rosa" minutesAway="10" patrolleeImage={Image} />
+      <PatrolToStart firstName="Victoria" lastName="Da Rosa" minutesAway="10" patrolleeImage={JaneDoeImage} />
       <FooterMap />
     </View>
   );
